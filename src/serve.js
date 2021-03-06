a413c03125b5e129a800258f9f5d1e0377841b12
maie421 <maie421@naver.com>
2021-03-07 01:47:55 +0900

    리펙토링

--- src/serve.js
+++ src/serve.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
-const mongoose = require('mongoose')
-const { User } = require('./models/User')
+const {userRouter}= require('./routes/userRoute');
+const mongoose = require('mongoose');
 
 const MONGO_URL ='mongodb+srv://admin:kh4CM8pSvYXjI6Dp@mongodb.pfyo1.mongodb.net/BlogService?retryWrites=true&w=majority';
 
@@ -10,78 +10,11 @@ const server = async() =>{
         await mongoose.connect(MONGO_URL,{useNewUrlParser: true,useUnifiedTopology: true,createIndexes:true,useFindAndModify:false});
         mongoose.set('debug',true);
         console.log('MongoDB connected');
-        //mongoose.connect(MONGO_URL).then(result => console.log({ result }))
+        app.use(express.json());
 
-        app.use(express.json()); 
+        app.use('/user',userRouter);
  
-        app.get('/user',async (req,res)=>{
-            try{
-                const users = await User.find({});
-                return res.send({users});
-            }catch(err){
-                console.log(err);
-                return res.status(500).send({err : err.message})
-            }
-        });
         
-        app.get("/user/:userId",async(req,res)=>{
-            try{
-                const { userId } = req.params;
-                if(!mongoose.isValidObjectId(userId)) return res.status(400).send({err:"invalid userid"})
-                const user = await User.findOne({_id:userId});
-                return res.send({user});
-            }catch(err){
-                console.log(err);
-                 return res.status(500).send('server listening on port 3000'); 
-            }
-        })
-
-        app.delete("/user/:userId",async(req,res)=>{
-            try{
-                const { userId } = req.params;
-                if(!mongoose.isValidObjectId(userId)) return res.status(400).send({err:"invalid userid"})
-                const user = await User.findByIdAndDelete({_id:userId});
-                return res.send({user});
-            }catch(err){
-                console.log(err);
-                return res.status(500).send('server listening on port 3000'); 
-            }
-        })
-        
-        app.put("/user/:userId",async(req,res)=>{
-            try{
-                const { userId } = req.params;
-                if(!mongoose.isValidObjectId(userId)) return res.status(400).send({err:"invalid userid"});
-                const { age, name }=req.body;
-                if(!age && !name) return res.status(400).send({err:"age is required"});
-                if(age && typeof age !== 'number') return res.status(400).send({err:"age must be a number"});
-                if(name && typeof name.first !== 'string' && typeof name.last !== 'string') return res.status(400).send({err : "first and last"});
-                let updateBody ={};
-                if(age) updateBody.age = age;
-                if(name) updateBody.name = name; 
-
-                const user = await User.findByIdAndUpdate(userId , { age , nums },{new:true});
-                return res.send({user});
-            }catch(err){
-                console.log(err);
-                return res.status(500).send('server listening on port 3000'); 
-            }
-        })
-        
-        app.post('/user',async (req,res)=>{
-            try{
-                let {username, name} = req.body;
-                if(!username) return res.status(400).send({err: "username is required"});
-                if(!name || !name.firest || !name.last) return res.status(400).send({ err: "both first and last names are required"});
-                const user= new User(req.body);
-                await user.save();
-                return res.send({seccuss:true});
-            }catch(err){
-                console.log(err);
-                return res.status(500).send('server listening on port 3000'); 
-            
-            }
-        });
 
 
         app.listen(3000,()=>console.log('3000번 연결 '));
