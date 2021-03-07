e8651e49d882a7444dae4d20b53b59f74dae0e3c
maie421 <maie421@naver.com>
2021-03-07 21:25:08 +0900

    blogroute

--- src/routes/blogRoute.js
+++ src/routes/blogRoute.js
@@ -1,20 +1,41 @@
 const { Router } = require('express');
+const { isValidObjectId } = require('mongoose');
 const blogRouter = Router();
 const {Blog} = require("../models/Blog");
+const { User } = require('../models/User');
 
 blogRouter.post('/', async(req,res)=>{
     try{
+        const { title, content, islive, userId } = req.body;
+        if (typeof title !== "string")
+          return res.status(400).send({ err: "title is required" });
 
-    }catch(err){
+        if (typeof content !== "string")
+          return res.status(400).send({ err: "content is required" });
+
+        if (islive && typeof islive !== "boolean")
+          return res.status(400).send({ err: "islive must be a boolean" });
+
+        if (!isValidObjectId(userId))
+          return res.status(400).send({ err: "userId is invalid" });
+
+        let user = await User.findById(userId);
+        if (!user) res.status(400).send({ err: "user does not exist" });
+    
+        let blog = new Blog({ ...req.body, user });
+        await blog.save();
+        return res.send({ blog });
+      } catch (err) {
         console.log(err);
-        res.status(500).send({err: err.message});
-    }
+        res.status(500).send({ err: err.message });
+      }
 });
 
 
 blogRouter.get('/', async(req,res)=>{
-    try{
-
+    try{ 
+      const blogs = await Blog.find({});
+      return res.send({blogs});
     }catch(err){
         console.log(err);
         res.status(500).send({err: err.message});
@@ -23,7 +44,13 @@ blogRouter.get('/', async(req,res)=>{
 
 blogRouter.get('/:blogId', async(req,res)=>{
     try{
+      const {blogId} = req.params;
 
+      if(!isValidObjectId(blogId))
+        res.status(400).send({err : err.message});
+
+      const blog = await Blog.findOne({_id:blogId});
+      return res.send({blog});
     }catch(err){
         console.log(err);
         res.status(500).send({err: err.message});
@@ -31,21 +58,49 @@ blogRouter.get('/:blogId', async(req,res)=>{
 });
 
 blogRouter.put('/:blogId', async(req,res)=>{
-    try{
+  try {
+    const { blogId } = req.params;
+    if (!isValidObjectId(blogId))
+      res.status(400).send({ err: "blogId is invalid" });
 
-    }catch(err){
-        console.log(err);
-        res.status(500).send({err: err.message});
-    }
+    const { title, content } = req.body;
+    if (typeof title !== "string")
+      res.status(400).send({ err: "title is required" });
+    if (typeof content !== "string")
+      res.status(400).send({ err: "content is required" });
+
+    const blog = await Blog.findOneAndUpdate(
+      { _id: blogId },
+      { title, content },
+      { new: true }
+    );
+    return res.send({ blog });
+  } catch (err) {
+    console.log(err);
+    res.status(500).send({ err: err.message });
+  }
 });
 
 blogRouter.patch('/:blogId/live', async(req,res)=>{
-    try{
+  try {
+    const { blogId } = req.params;
+    if (!isValidObjectId(blogId))
+      res.status(400).send({ err: "blogId is invalid" });
 
-    }catch(err){
-        console.log(err);
-        res.status(500).send({err: err.message});
-    }
+    const { islive } = req.body;
+    if (typeof islive !== "boolean")
+      res.status(400).send({ err: "boolean islive is required" });
+
+    const blog = await Blog.findByIdAndUpdate(
+      blogId,
+      { islive },
+      { new: true }
+    );
+    return res.send({ blog });
+  } catch (err) {
+    console.log(err);
+    res.status(500).send({ err: err.message });
+  }
 });
 
 module.exports = {blogRouter};
